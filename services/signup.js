7f5496af660c1d97ef4b87b96fb944a256cd04a9
dimah02 <dimahmehdawi@gmail.com>
2024-11-22 14:15:43 +0300

    update signup function

--- services/signup.js
+++ services/signup.js
@@ -7,7 +7,7 @@ const { generateToken } = require("../utils/jwtUtils");
 
 async function createUser(userData) {
     try {
-        const { name, email, password, blood_type, gender, birth_date, allergy_to_medications, medical_history, height, weight } = userData;
+        const {email,password,name} = userData;
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             throw new Error("User already exists");
@@ -18,13 +18,6 @@ async function createUser(userData) {
             name,
             email,
             password: hashedPassword,
-            blood_type,
-            gender,
-            birth_date,
-            allergy_to_medications,
-            medical_history,
-            height,
-            weight
         });
         const savedUser = await newUser.save();
         const token = generateToken(savedUser);
@@ -32,13 +25,6 @@ async function createUser(userData) {
             id: savedUser._id,
             name:savedUser.name,
             email:savedUser.email,
-            blood_type: savedUser.blood_type,
-            gender: savedUser.gender,
-            birth_date: savedUser.birth_date,
-            allergy_to_medications: savedUser.allergy_to_medications,
-            medical_history: savedUser.medical_history,
-            height: savedUser.height,
-            weight: savedUser.weight,
             token
         };
     }
